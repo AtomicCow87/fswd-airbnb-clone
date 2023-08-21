6c0ee25be50abfb94f4a843888c94f2cd94008fb
Zach <zkdoran@gmail.com>
2023-08-21 17:46:10 -0400

    copy home to render properties

--- app/javascript/src/user/user.jsx
+++ app/javascript/src/user/user.jsx
@@ -8,12 +8,18 @@ class User extends React.Component {
   state = {
     authenticated: false,
     username: null,
+    user_id: null,
+    properties: [],
+    total_pages: null,
+    next_page: null,
+    loading: true,
   }
 
   componentDidMount() {
     fetch('/api/authenticated')
       .then(handleErrors)
       .then(data => {
+        console.log(data)
         this.setState({ 
           authenticated: data.authenticated,
           username: data.username,
@@ -21,10 +27,38 @@ class User extends React.Component {
       })
   }
 
-  
+  getProperties = () => {
+    fetch(`/api/properties/${this.state.username}?page=1`)
+      .then(handleErrors)
+      .then(data => {
+        this.setState({
+          properties: data.properties,
+          total_pages: data.total_pages,
+          next_page: data.next_page,
+          loading: false,
+        })
+      })
+  }
+
+  loadMore = () => {
+    if (this.state.next_page === null) {
+      return;
+    }
+    this.setState({ loading: true });
+    fetch(`/api/properties/${this.state.username}?page=${this.state.next_page}`)
+      .then(handleErrors)
+      .then(data => {
+        this.setState({
+          properties: this.state.properties.concat(data.properties),
+          total_pages: data.total_pages,
+          next_page: data.next_page,
+          loading: false,
+        })
+      })
+  }
 
   render() {
-    const { authenticated, username } = this.state;
+    const { authenticated, username, properties, next_page, loading } = this.state;
 
     let page = window.location.pathname.replace('/user/', '')
 
@@ -64,13 +98,27 @@ class User extends React.Component {
             </div>
           </div>
           <div className="row">
-            <div className="col pb-5">
-              <h3>Box One</h3>
+            {properties.map(property => {
+              return (
+                <div key={property.id} className="col-6 col-lg-4 mb-4 property">
+                  <a href={`/property/${property.id}`} className="text-body text-decoration-none">
+                    <p className="text-uppercase mb-0 text-secondary"><small><b>{property.city}</b></small></p>
+                    <h6 className="mb-0">{property.title}</h6>
+                    <p className="mb-0"><small>${property.price_per_night} USD/night</small></p>
+                  </a>
+                </div>
+              )
+            })}
+          </div>
+          {loading && <p>loading...</p>}
+          {(loading || next_page === null) ||
+            <div className="text-center">
+              <button
+                className="btn btn-light mb-4"
+                onClick={this.loadMore}
+              >load more</button>
             </div>
-            <div className="col pb-5">
-              <h3>Box Two</h3>
-            </div>
-          </div> 
+          } 
         </div>
       </Layout>
     )
