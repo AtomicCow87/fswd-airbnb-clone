a937242eba5aa8ef672bd45bed1ccb565835fe99
Zach <zkdoran@gmail.com>
2023-08-21 16:48:40 -0400

    user verification setup

--- app/javascript/src/user/user.jsx
+++ app/javascript/src/user/user.jsx
@@ -21,21 +21,59 @@ class User extends React.Component {
       })
   }
 
+  
+
   render() {
     const { authenticated, username } = this.state;
+
+    let page = window.location.pathname.replace('/user/', '')
+
     if (!authenticated) {
       return (
         <Layout>
-          <h1>Log in to view this page</h1>
+          <div className="border p-4 mb-4">
+            Please <a href={`/login?redirect_url=${window.location.pathname}`}>log in</a> to see.
+          </div>
         </Layout>
-      )
-    } else {
+      );
+    }
+
+    if (page != username) {
       return (
         <Layout>
-          <h1>{username}</h1>
+          <div className="container">
+            <div className="row">
+              <div className="col-12 col-md-6 offset-md-3 py-5">
+                <h1>Hmmm... You seem to be in the wrong place.</h1>                
+              </div>
+              <div className="col-12 col-md-6 offset-md-3 pb-5">
+                <h3>Try going to your <a href={`/user/${username}`}>profile</a> instead.</h3>
+              </div>
+            </div>
+          </div>
         </Layout>
       )
     }
+      
+    return (
+      <Layout>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-auto py-5">
+              <h1>Welcome, {username}!</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col pb-5">
+              <h3>Box One</h3>
+            </div>
+            <div className="col pb-5">
+              <h3>Box Two</h3>
+            </div>
+          </div> 
+        </div>
+      </Layout>
+    )
   }
 }
 
