975672c6945d68f926bcb1013a2a43a9411ce787
Zach <zkdoran@gmail.com>
2023-08-22 12:53:52 -0400

    removed unused

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,6 @@ Rails.application.routes.draw do
 
     get '/properties/:id/bookings' => 'bookings#get_property_bookings'
     get '/authenticated' => 'sessions#authenticated'
-    get '/properties/index_by_user' => 'properties#index_by_user'
 
     # stripe webhook
     post '/charges/mark_complete' => 'charges#mark_complete'
